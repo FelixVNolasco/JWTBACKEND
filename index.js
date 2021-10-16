9ba4eec1967513026321662cf64fe487d93d147f
Felix Enrique Vega Nolasco <49852681+FelixVNolasco@users.noreply.github.com>
2021-10-16 18:31:21 -0500

    eslint

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const { dbConnection } = require('./db/config');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const { dbConnection } = require("./db/config");
+require("dotenv").config();
 
 // Crear el servidor/aplicación de express
 const app = express();
@@ -9,22 +9,18 @@ const app = express();
 // Base de datos
 dbConnection();
 
-
 // Directorio Público
-app.use( express.static('public') );
+app.use(express.static("public"));
 
 // CORS
-app.use( cors() );
+app.use(cors());
 
 // Lectura y parseo del body
-app.use( express.json() );
-
+app.use(express.json());
 
 // Rutas
-app.use( '/api/auth', require('./routes/auth') );
+app.use("/api/auth", require("./routes/auth"));
 
-
-app.listen( process.env.PORT, () => {
-    console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
+app.listen(process.env.PORT, () => {
+  console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
 });
-
--- middlewares/validar-jwt.js
+++ middlewares/validar-jwt.js
@@ -1,40 +1,32 @@
-const { response } = require('express');
-const jwt = require('jsonwebtoken');
+const { response } = require("express");
+const jwt = require("jsonwebtoken");
 
+const validarJWT = (req, res = response, next) => {
+  const token = req.header("x-token");
 
-const validarJWT = ( req, res = response, next ) => {
+  if (!token) {
+    return res.status(401).json({
+      ok: false,
+      msg: "error en el token",
+    });
+  }
 
-    const token = req.header('x-token');
+  try {
+    //PAYLOAD
+    const { uid, name } = jwt.verify(token, process.env.SECRET_JWT_SEED);
+    req.uid = uid;
+    req.name = name;
+  } catch (error) {
+    return res.status(401).json({
+      ok: false,
+      msg: "Token no válido",
+    });
+  }
 
-    if( !token  ) {
-        return res.status(401).json({
-            ok: false,
-            msg: 'error en el token'
-        });
-    }
-
-    try {
-        //PAYLOAD
-        const { uid, name } = jwt.verify( token, process.env.SECRET_JWT_SEED );
-        req.uid  = uid;
-        req.name = name;
-
-        
-    } catch (error) {
-        return res.status(401).json({
-            ok: false,
-            msg: 'Token no válido'
-        });
-    }
-
-
-
-    // TODO OK!
-    next();
-}
-
+  // TODO OK!
+  next();
+};
 
 module.exports = {
-    validarJWT
-}
-
+  validarJWT,
+};
