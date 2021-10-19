4895bd3955f07f4b20f810d5eed71beb12262362
Felix Enrique Vega Nolasco <49852681+FelixVNolasco@users.noreply.github.com>
2021-10-19 16:22:42 -0500

    make it better

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const { dbConnection } = require("./db/config");
+const path = require('path');
+
 require("dotenv").config();
 
 // Crear el servidor/aplicación de express
@@ -20,6 +22,12 @@ app.use(express.json());
 
 // Rutas
 app.use("/api/auth", require("./routes/auth"));
+
+//Demas rutas
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public/index.html'));
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
